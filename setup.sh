5e237d8b88e2a83c60b160d8015dce00f0563807
Dharunya05 <dharubuji06@gmail.com>
2025-05-19 10:54:40 -0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-minikube start --driver=docker
-kubectl apply -f rbac.yaml
-kubectl apply -f network-policy.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f prometheus-deployment.yaml
-kubectl apply -f grafana-deployment.yaml
